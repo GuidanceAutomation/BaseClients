59b4cb25f42cc7b8e627ba90334e0aea10077fc0
Martin Davies <martin.davies@guidanceautomation.com>
2020-09-07 12:25:49 +0100

    Comments all public code.

--- src/BaseClients.Architecture/IClient.cs
+++ src/BaseClients.Architecture/IClient.cs
@@ -4,10 +4,13 @@ using System.ServiceModel;
 
 namespace BaseClients.Architecture
 {
+    /// <summary>
+    /// Clients enabled remote procedure calls to be made on the server which may or may not return a value.
+    /// </summary>
     public interface IClient : IDisposable
     {
         /// <summary>
-        /// Endpoint address of the service host
+        /// Endpoint address of the service host (server configuration).
         /// </summary>
         EndpointAddress EndpointAddress { get; }
 
